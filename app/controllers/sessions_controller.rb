ec70372342b2241727272dbcd3e66d13ca2d129d
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 17:05:47 -0600

    Update /login
    Login works on frontend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    
-  end 
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id]
+      render json: user
+    else
+      render json: { errors: ['Username or password incorrect'] }, status: :unauthorized
+    end
+  end
 end
