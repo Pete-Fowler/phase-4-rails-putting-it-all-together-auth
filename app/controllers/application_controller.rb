eb6511aa80e78deb373b0d787b4d57d98b1a7ea6
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 16:50:43 -0600

    Remove application controller auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-
-  before_action :authorize
-
-  def authorize
-    render json: { errors: ['Not authorized'] }, status: :unauthorized unless session.include? :user_id
-  end
+ 
 end
