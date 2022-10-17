487fb318c07a02d24e9efcda650d448bcce94d68
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 16:20:19 -0600

    Pass user tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id]
+      render json: user, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  private 
+
+  def user_params 
+    params.permit(:username, :password, :image_url, :bio)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :recipes
-  resources :users
+ 
+  post '/signup', to: 'users#create'
 end
